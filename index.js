4cd94dfd398df45ce9621dada94a415dccb0e229
Mathias Buus <mathiasbuus@gmail.com>
2024-03-10 18:27:50 +0100

    add simply mem storage

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const ReadyResource = require('ready-resource')
 const b4a = require('b4a')
 const binding = require.addon()
 
+const EMPTY = Buffer.alloc(0)
+
 module.exports = class BareSQLite3 extends ReadyResource {
   constructor (name) {
     super()
     this.name = name
 
     this._handle = b4a.allocUnsafe(binding.sizeof_bare_sqlite3_t)
+    this._files = new Array(8)
   }
 
   _open () {
@@ -24,12 +27,34 @@ module.exports = class BareSQLite3 extends ReadyResource {
     binding.bare_sqlite3_close(this._handle)
   }
 
-  _onVFSRead (filetype, buf, offset) {
-    console.log('JS READ:', { filetype, buf, offset })
+  _onVFSRead (filetype, arrayBuffer, offset) {
+    const buffer = Buffer.from(arrayBuffer)
+
+    let stored = (this._files[filetype - 1] || EMPTY).subarray(offset, offset + buffer.byteLength)
+    if (stored < buffer.byteLength) stored = Buffer.concat([stored, Buffer.alloc(buffer.byteLength - stored.byteLength)])
+
+    buffer.set(stored, 0)
+
+    console.log('JS READ:', { filetype, offset, byteLength: buffer.byteLength })
   }
 
-  _onVFSWrite (filetype, buf, offset) {
-    console.log('JS WRITE:', { filetype, buf, offset })
+  _onVFSWrite (filetype, arrayBuffer, offset) {
+    const buffer = Buffer.from(arrayBuffer)
+    const size = buffer.byteLength + offset
+
+    let stored = this._files[filetype - 1] || Buffer.alloc(4096)
+
+    let storedSize = stored.byteLength
+    while (storedSize < size) storedSize *= 2
+
+    if (stored.byteLength < storedSize) {
+      stored = Buffer.concat([stored, Buffer.alloc(storedSize - stored.byteLength)])
+    }
+
+    this._files[filetype - 1] = stored
+    stored.set(buffer, offset)
+
+    console.log('JS WRITE:', { filetype, offset, byteLength: buffer.byteLength })
   }
 
   _exec (query) {
